64c30ba13fa99c51a98632dadd1635ca0d676720
pverspeelt <phiver4@gmail.com>
2018-07-19 14:00:18 +0200

    First roxygen documentation added
    Built package and added documentation to stochRSI. This also created the man folder to store the documentation files. Roxygen adjusted all the needed other files.

--- R/Technical Indicators.R
+++ R/Technical Indicators.R
@@ -1,19 +1,31 @@
-# matches IB's stoch RSI
-# Needs a minimum of (2 * n) 28 days of price action before the numbers start 
-# working, but this is about 2.5 times faster than using 
-# (RSI(adm$close, 14) - runMin(RSI(adm$close, 14))) / (runMax(RSI(adm$close, 14)) - runMin(RSI(adm$close, 14)))
-# and gives only 4 days extra info. 
-
-# price Price series that is coercible to xts or matrix
-# n Number of periods
+#' Stochastic RSI
+#' 
+#' This stochastic RSI matches IB's stoch RSI.
+#' Needs a minimum of (2 * n) 28 days of price action before the numbers start 
+#' working, but this is about 2.5 times faster than using
+#' (RSI(adm$close, 14) - runMin(RSI(adm$close, 14))) / (runMax(RSI(adm$close, 14)) - runMin(RSI(adm$close, 14)))
+#' and gives only 4 days extra info. 
+#' 
+#' It is a measure of RSI relative to its own high/low range over a user defined period of time. 
+#' The Stochastic RSI is an oscillator that calculates a value between 0 and 1 which can be plotted as a line. 
+#' This indicator is primarily used for identifying overbought and oversold conditions.
+#' 
+#' @param price Price series that is coercible to xts or matrix
+#' @param n Number of periods. Default is 14.
+#'
+#' @return The stochRSI function will return a timeseries with values ranging between 0 and 1
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' getSymbols("ADM", from = "2018-01-01", to = "2018-07-01")
+#' chartSeries(ADM, TA = NULL) # without volume
+#' addTA(stochRSI(Cl(ADM)))
+#' }
 stochRSI <- function(price, n = 14L){
   rsi <- RSI(price, n)
   rsi_out <- (rsi - runMin(rsi, n)) / (runMax(rsi, n) - runMin(rsi, n))
   return(rsi_out)
 }
 
-# example
-# getSymbols("ADM", from = "2018-01-01", to = "2018-07-01")
-# chartSeries(ADM, TA = NULL) # without volume
-# addTA(stochRSI(Cl(ADM)))
 
