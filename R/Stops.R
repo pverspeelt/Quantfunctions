2a2c02bf36334cc1c906a0f18444709ae92811cd
pverspeelt <phiver4@gmail.com>
2018-07-21 15:05:16 +0200

    add safezone stop
    This commit adds the safezone stop to the Stops.R file and adds the relevant documentation.

--- R/Stops.R
+++ R/Stops.R
@@ -36,3 +36,64 @@ chandelier <- function(HLC, n = 22, coef = 3, trend = "up"){
   }
   return(chandelier)
 }
+
+
+
+#' Safezone Stop
+#' 
+#' The safezone stop was developed by Alexander Elder. The safezone stop is described in his book "Come Into My Trading Room". 
+#' 
+#' Once you have defined a trend, you need to choose the length of the lookback period for measuring noise level. 
+#' It has to be long enough to track recent behavior but short enough to be relevant for current trading. 
+#' 
+#' If the trend is up, mark all downside penetrations during the lookback period, 
+#' add their depths, and divide the sum by the number of penetrations. This gives you
+#' the average downside penetration for the selected lookback period. Multiply 
+#' the average downside penetration by a coefficient, to place stops farther away from the market 
+#' than the average level of noise. The results are subtracted from yesterday's low to create the stop. 
+#' To prevent the stop from lowering too fast, the prevent lookback option will keep the maximum calculated 
+#' stop for the specified lookback. The rules are reversed for down trend calculations.
+#'  
+#'
+#' @param HL An object that is coercible to xts or matrix and contains High-Low prices.
+#' @param n Number of lookback periods for the safezone period. Default is 10.
+#' @param coef Coefficient for multiplying the average downside (or upside) penetration. Default is 2.
+#' @param prevent Prevents the stop from lowering for x days. Default is 5.
+#' @param trend Indicates if the safezone stop should be calculated for the up-trend or down-trend. 
+#' Default is "up". Possible values are "up" or "down".
+#'
+#' @return Returns the safezone exit points which can be used as stop loss in a trend following strategy.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # show safezone stop on chart
+#' getSymbols("ADM", from = "2018-01-01", to = "2018-07-01")
+#' chartSeries(ADM)
+#' addTA(safezone(ADM), on = 1)
+#' }
+safezone <- function(HL, n = 10, coef = 2, prevent = 5, trend = "up"){
+  
+  if(!trend %in% c("up", "down")) stop("trend should be up or down", call. = FALSE)
+  
+  if(trend == "down"){
+    high <- Hi(HL)
+    up_pen <- ifelse(high > quantmod::Lag(high), high - quantmod::Lag(high), 0)
+    up_sum <- TTR::runSum(up_pen, n = n)
+    up_sumYN <- TTR::runSum(up_pen > 0)
+    up_avg <- ifelse(up_sumYN == 0, 0, up_sum / up_sumYN)
+    up_stop <- quantmod::Lag(high) + coef * quantmod::Lag(up_avg)
+    protection <- TTR::runMin(up_stop, n = prevent)
+  } else {
+    low <- Lo(HL)
+    down_pen <- ifelse(quantmod::Lag(low) > low, quantmod::Lag(low) - low, 0)
+    down_sum <- TTR::runSum(down_pen, n = n)
+    down_sumYN <- TTR::runSum(down_pen > 0)
+    down_avg <- ifelse(down_sumYN == 0, 0, down_sum / down_sumYN)
+    short_stop <- quantmod::Lag(low) - coef * quantmod::Lag(down_avg)
+    protection <- TTR::runMax(short_stop, n = prevent)
+  }
+  
+  return(setNames(protection, "stop"))
+}
+
