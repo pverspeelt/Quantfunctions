ed4f0a5c77c8bbd4540a60710c6e9d179ddd6e68
pverspeelt <phiver4@gmail.com>
2018-12-29 16:37:25 +0100

    Create heikin_ashi function
    All changes in this commit will:
    Create Heikin-Ashi function
    Create Heikin-Ashi documentation
    Update to Namespace for new Heikin-Ashi function
    Increase version number to 0.0.1.0 in description

---
+++ R/heikin_ashi.R
@@ -0,0 +1,60 @@
+#' Heikin-Ashi
+#' 
+#' Calculates the Heikin-Ashi values needed for plotting in a candlestick chart.
+#' 
+#'  1. The Heikin-Ashi Close is an average of the open, high, low and 
+#'  close for the current period. 
+#'  
+#'  2. The Heikin-Ashi Open is the average of the prior Heikin-Ashi open and the 
+#'  prior Heikin-Ashi close
+#'  
+#'  3. The Heikin-Ashi High is the maximum of: 
+#'  the current period's high, the current Heikin-Ashi open or the current 
+#'  Heikin-Ashi close.
+#'  
+#'  4. The Heikin-Ashi low is the minimum : 
+#'  the current period's low, the current Heikin-Ashi open or the current 
+#'  Heikin-Ashi close.
+#'  
+#'  More information can be found at \href{https://www.investopedia.com/trading/heikin-ashi-better-candlestick/}{Investopedia}
+#'
+#' @param data an xts object that contains OHLC data 
+#'
+#' @return The heikin_ashi function will return a OHLC dataset with the calculated 
+#' heikin ashi values needed for plotting in a candlestick chart.
+#' 
+#' 
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ADM <- getSymbols("ADM", 
+#'                   from = "2018-01-01", 
+#'                   to = "2018-07-01", 
+#'                   auto.assign = FALSE)
+#' ha_ADM <- heikin_ashi(ADM)
+#' chartSeries(ha_ADM)
+#' }
+#'  
+heikin_ashi <- function(data) {
+  
+  if(!quantmod::is.OHLC(data)) stop("data must contain OHLC columns")
+  
+  heikin_close <- xts::xts(Matrix::rowMeans(quantmod::OHLC(data)), order.by = index(data))
+  heikin_open  <- quantmod::Op(data)
+  
+  # need a loop: heiki ashi open is dependent on the previous value
+  for(i in 2:nrow(data)) {
+    heikin_open[i] <- (heikin_open[i-1] + heikin_close[i-1]) / 2
+  }
+  
+  heikin_high <- xts::xts(apply(cbind(quantmod::Hi(data), heikin_open, heikin_close), 1, max), order.by = index(data))
+  heikin_low <- xts::xts(apply(cbind(quantmod::Lo(data), heikin_open, heikin_close), 1, min), order.by = index(data))
+  
+  out <- merge(heikin_open, heikin_high, heikin_low, heikin_close)
+  out <- setNames(out, c("Open", "High", "Low", "Close"))
+  
+}
+
+
+
